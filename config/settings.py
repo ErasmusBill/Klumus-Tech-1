fbddd627e6585d7a7a7e234e951fd76812610f20
erasmus <erasmuschawey12345@gmail.com>
2025-10-19 02:08:57 +0000

    Prepare for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ========================
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 
 # Render provides your domain automatically
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", os.getenv("RENDER_EXTERNAL_HOSTNAME")]

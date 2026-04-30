15378f33b3d7a5094be394ff4c7d795814b5c27b
Erasmus Bill <erasmuschawey12345@gmail.com>
2026-04-30 23:28:53 +0000

    fix urls.py to render images in production

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +28,11 @@ urlpatterns = [
     path("student/",include("student.urls")),
     path('select2/', include('django_select2.urls')),
     path("ai/",include("ai_predictor.urls")),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    # Serve uploaded media files even when DEBUG=False.
+    # This prevents broken logos/profile images when the reverse proxy
+    # does not have a dedicated /media alias.
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
